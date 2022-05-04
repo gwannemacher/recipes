c3778d61bdf0d73ea01c914db461f37851187021
Grace Wannemacher <gwannemacher@gmail.com>
2022-05-03 21:48:35 -0400

    more cleanup

--- client/src/components/form/NameField.tsx
+++ client/src/components/form/NameField.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import Stack from '@mui/material/Stack';
 
 const NameField = (props) => {
     return (
